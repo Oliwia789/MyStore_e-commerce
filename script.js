a0ad4670ee2452208276744b70fd51b1f1a9b90b
Oliwia Rutkowska <oliwia.r789@gmail.com>
2022-02-11 19:09:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+    if(scrollY >= 180) {
+        navbar.classList.add("bg");
+    } else {
+        navbar.classList.remove("bg");
+    }
+})
+
+const collageImg = [...document.querySelectorAll('.collage-img')];
+
+collageImg.map((item, i) => {
+    item.addEventListener("mouseover", () => {
+        collageImg.map((image, index) => {
+            if(index != i){
+                image.style.filter = `blur(10px)`;
+                item.style.zIndex = 2;
+            }
+        })
+    })
+
+    item.addEventListener("mouseleave", () => {
+        collageImg.map((image, index) => {
+            image.style = null;
+        })
+    })
+})
